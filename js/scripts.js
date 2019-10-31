9e90def492d4224ef1831b6cba414995011e385c
Student <example@example.com>
2019-10-31 10:29:55 -0700

    Added output

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
-  $("form#list").submit(function(event) {
+  $("form#num").submit(function(event) {
+    var num = $("#input").val();
 
+    var output = (toRoman(num).join(''));
 
+    if(!output){
+      output="Stultus es valde.";
+    }
+    $(".result").text(output);
     event.preventDefault();
   });
 });
+
+
+
 function toRoman(num) {
-  var chars = [['','I','II','III','IV','V','VI','VII','VIII','IX'],['','X','XX','XXX','XL','L','LX','LXX','LXXX','XC'],['','C','CC','CCC','CD','D','DC','DCC','DCCC','CM'],['','M','MM','MMM']]
+  num = num.replace ( /[^0-9]/g, '' );
+  if (!(parseInt(num)/parseInt(num)===1)) {
+    return [''];
+  }
+  var chars = [['','I','II','III','IV','V','VI','VII','VIII','IX'],['','X','XX','XXX','XL','L','LX','LXX','LXXX','XC'],['','C','CC','CCC','CD','D','DC','DCC','DCCC','CM'],['','M','MM','MMM','MMMM','MMMMM','MMMMMM','MMMMMM','MMMMMMM','MMMMMMMM','MMMMMMMMM']]
   var output=[];
   var j = 0;
   for (var i=((num.length)-1); i>-1;i--){
